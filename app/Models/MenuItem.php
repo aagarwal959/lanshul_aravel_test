8e0eec630fb4e57f6c4b15acb8bede15abe19021
Anshul Agarwal <aagarwal959@gmail.com>
2022-05-27 19:14:41 +0530

    Third question

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,11 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+	public function children() {
+		return $this->hasMany(MenuItem::class, "parent_id", "id")->with("children");
+	}
+	public static function getMenu($value='')
+	{
+		return self::with("children")->whereNull("parent_id")->get();
+	}
 }
