2318461d56db8af8606f3e3cb8193fc79f1096f1
Anshul Agarwal <aagarwal959@gmail.com>
2022-05-27 18:41:34 +0530

    first QUESTION

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+	protected $connection = "mysql";
+
+	public static function getallEventWorkShops(){
+		$events = new Event;
+
+		return $events->with("workshops")->get();
+	}
+
+	public function workshops() {
+		return $this->hasMany(Workshop::class, "event_id", 'id');
+	}
 }
